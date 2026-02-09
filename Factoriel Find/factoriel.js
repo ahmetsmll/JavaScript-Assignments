4d7cea1217bfe9d045cd3aa85b2fd2d58cc08bd7
Ahmed Şamil Karadeniz <ahmetsamil44@gmail.com>
2026-02-10 00:12:06 +0300

    Add files via upload

---
+++ Factoriel Find/factoriel.js
@@ -0,0 +1,6 @@
+let number= Number(prompt("Please enter a number"));
+let multipilacation=1;
+for(let i=1;i<=number;i++){
+    multipilacation= multipilacation*i;
+}
+alert(number+"! = "+multipilacation);
