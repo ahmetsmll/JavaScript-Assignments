953fb948b182f8c8342fc3d06e99e25783a210b9
Ahmed Şamil Karadeniz <ahmetsamil44@gmail.com>
2026-02-11 23:33:25 +0300

    Add files via upload

---
+++ Binary to Decimal/binToDec.js
@@ -0,0 +1,14 @@
+let binary = prompt("please enter a number in binary type (0 - 1)");
+let decimal = binaryToDecimal(binary);
+alert(binary+" = "+ decimal);
+
+
+function binaryToDecimal(binary){
+    let sum =0;
+
+    for(let i = 0; i<=binary.length-1;i++){
+        sum = sum + ( Number(binary.charAt(i)) ) * (2**(binary.length-1-i));
+        
+    }
+    return sum ;
+}
